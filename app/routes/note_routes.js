059d945543eaad7263fc87c98b558a991fd7983e
Patti Rock <37083207+pattirock@users.noreply.github.com>
2018-11-18 19:20:42 +0100

    Tutorial files

---
+++ app/routes/note_routes.js
@@ -0,0 +1,57 @@
+// routes/note_routes.js
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports = function(app, db) {
+  // DELETE
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Note ' + id + ' deleted!');
+      }
+    });
+  });
+
+  // GET
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
+  // POST
+  app.post('/notes', (req, res) => {
+    // You'll create your note here.
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+
+  // PUT
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(note);
+      }
+    });
+  });
+};
